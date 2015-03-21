7f7229c39135923d3de9ece2adc9ac487304e3c4
Bear Grylls <anwilliams93@gmail.com>
2015-03-21 20:30:18 +0000

    Got the hardcoded get working

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -29,4 +29,4 @@ def get_tasks():
     return jsonify({'funruns': funruns})
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, debug=True)
