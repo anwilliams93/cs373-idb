f7f7f7afb9bcfa542e56c3a829c2985f227ff15b
David Aguirre <da9492@cs.utexas.edu>
2015-03-21 13:55:09 -0500

    testing another page

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello Kurt!'
+
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html') 
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
