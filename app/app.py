15de65568b44321655b41ed52800ab4ad011d19a
Bear Grylls <anwilliams93@gmail.com>
2015-03-25 20:56:16 +0000

    Changes...?

--- app/app.py
+++ app/app.py
@@ -82,6 +82,7 @@ funruns = [
         'description':  u'Crash, smash, and splash your way through a 5k course with larger-than-life obstacles and elements inspired by the hit TV show Wipeout! Take on the infamous Big Balls, Sweeper, Wrecking Balls, and Happy Endings! Hilarious thrills and magnificent spills await!',
         'map_url':      u'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12352.756020060675!2d-76.621608!3d39.283964!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2428d93f0397c539!2sOriole+Park+at+Camden+Yards!5e0!3m2!1sen!2sus!4v1427230426235',
         'video_url':    u'https://www.youtube.com/embed/1uOII9K5c0c',
+        'fb_url':       u'https://www.facebook.com/WIPEOUTRUN',
         'imgs':         [u'/static/img/runTempl/wipeout1.jpg', u'/static/img/runTempl/wipeout2.jpg', u'/static/img/runTempl/wipeout3.jpg']
     },  
     {
@@ -99,6 +100,7 @@ funruns = [
         'description':  u'The movie producers must have been runners, because the distance between the former Higbee’s Department Store and the A Christmas Story House & Museum is about 5K. Ralphie’s dad, The Old Man must have used the 1938 Oldsmobile to track the distance No GPS in the 1940′s. To accommodate both movie locations that made this race famous, the distance for the races are approximate, and perhaps a little shorter or longer.',
         'map_url':      u'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2988.260792402495!2d-81.694473!3d41.498622999999995!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8830f07e4562d241%3A0x94e8396bc5227630!2sRenaissance+Cleveland+Hotel!5e0!3m2!1sen!2sus!4v1427230544965',
         'video_url':    u'https://www.youtube.com/embed/uPiN-_p7q2k',
+        'fb_url':       u'https://www.facebook.com/AChristmasStoryRun',
         'imgs':         [u'/static/img/runTempl/christmasstory1.jpg', u'/static/img/runTempl/christmasstory2.jpg', u'/static/img/runTempl/christmasstory3.jpg']
     },
     {
@@ -116,25 +118,26 @@ funruns = [
         'description':  u'The Dallas Turkey Trot really proves that everything\'s bigger in Texas. One of the largest multi-event races in the country, this trot attracts elite runners and regular ol\' birds alike. In fact, in 2011, it set a world record for the largest gathering of people dressed as turkeys.',
         'map_url':      u'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13418.251274220785!2d-96.79709!3d32.777333!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc83854fdfdad6162!2sDallas+City+Hall!5e0!3m2!1sen!2sus!4v1427228610474',
         'video_url':    u'https://www.youtube.com/embed/qdnzjhWgCOg',
+        'fb_url':       u'https://www.facebook.com/DallasYMCATrot',
         'imgs':         [u'/static/img/runTempl/dallasturkey1.jpg', u'/static/img/runTempl/dallasturkey2.jpg', u'/static/img/runTempl/dallasturkey3.jpg']
     }
 ]
 
 themes = [
     {
-        'id':           1,
+        'id':           0,
         'name':         u'Holiday',
         'buzzwords':    u'Christmas, Thanksgiving, Easter, Valentine\'s Day, St. Patrick\'s Day, New Year\'s, Halloween, 4th of July',
         'description':  u'Instead of sitting around the fire, talking of presents and Santa, why not go for a run with the family? Holiday runs are a great way to get out and celebrate during the festivities! Often featuring thematic competitions and costumes, they\re a sure way of making any holiday a memorable one.'
     },
     {
-        'id':           2,
+        'id':           1,
         'name':         u'Intense',
         'buzzwords':    u'Training, Diet, Hardcore, Recovery, Cutthroat',
         'description':  u'More interested in proving how much you can take than how much fun you\'ll actually have? Intense runs are exactly what you need - a good dose of body-numbing, soul-crushing exercise to show your stuff. See if you can survive the brutal difficulty of these events.'
     },
     {
-        'id':           3,
+        'id':           2,
         'name':         u'Costume',
         'buzzwords':    u'Silly, Uncomfortable, Dress-Up, Make-Believe',
         'description':  u'Ever run a 5K in a hot and heavy mascot uniform? How about amongst hundreds of other runners in similarly ridiculous outfits? Try a costume run to experience the unique challenge of running in something other than athletic clothes and join a hodge-podge mob of other costumed crazies!'
@@ -143,20 +146,23 @@ themes = [
 
 challenges = [
     {
-        'id':           1,
-        'name':         u'Running On Odd Ground',
+        'id':           0,
+        'name':         u'Running On Awkward Ground',
+        'difficulty':   60,
         'flavors':      u'Ice, Inflatable Balls, Mud',
         'description':  u'Think running is hard enough? Try running on mud, inflatable balls, or slippery surfaces! Just be ready for the fall - and to get back up again to keep trying.'
     },
     {
-        'id':           2,
+        'id':           1,
         'name':         u'Running In A Costume',
-        'flavors':      u'Mascot Costumes, Halloween Outfits, Tutus, Nude, Banana Outfit',
+        'difficulty':   40,
+        'flavors':      u'Mascots, Nude, Speedos, Costumes',
         'description':  u'Ideally, running should be done in comfortable shorts and shirt. Instead, some dare to run in costumes - or a lack of costume - in self expression and, oftentimes, silliness.'
     },
     {
-        'id':           3,
+        'id':           2,
         'name':         u'Running In Cold Weather',
+        'difficulty':   80,
         'flavors':      u'Snow, Ice, Freezing Temperatures',
         'description':  u'Take on the cold fearlessly with fun runs in less than ideal temperatures. Will the icy winds get to you or will you make it to the finish line and prevail?'
     }
