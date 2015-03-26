557a36d9772bafdad0cbc0ebb669680deb5160eb
Kurt Bixby <kurtbixby@utexas.edu>
2015-03-26 12:46:55 -0500

    Changed all references to runs to funruns

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify, Blueprint
 import api_helpers
 
-runs_api = Blueprint('runs_api', __name__, url_prefix='/api')
+funruns_api = Blueprint('funruns_api', __name__, url_prefix='/api')
 
 
 '''
@@ -10,7 +10,7 @@ ASSUME THAT ALL PARAMETERS EXCEPT MIN and MAX CAN BE USED ANY NUMBER OF TIMES
 
 # Root Section of API
 
-@runs_api.route('/', methods = ['GET'])
+@funruns_api.route('/', methods = ['GET'])
 def get_entry_points():
 	root_urls = api_helpers.retrieve_entry_points()
 	return jsonify({'urls': root_urls})
@@ -19,52 +19,51 @@ def get_entry_points():
 
 run_query_parameters = {'theme', 'challenge', 'location', 'min_price', 'max_price', 'min_length', 'max_length'}
 
-@runs_api.route('/runs/', methods = ['GET'])
-def get_runs():
+@funruns_api.route('/funruns/', methods = ['GET'])
+def get_funruns():
 	filtered_params = api_helpers.filter_query_parameters(run_query_parameters, request.args)
 	
-	# ASSUMING RUNS IS THE ENTIRE LIST OF RUNS
-	runs = api_helpers.retrieve_runs()
+	# ASSUMING FUNRUNS IS THE ENTIRE LIST OF FUNRUNS
+	funruns = api_helpers.retrieve_funruns()
 
-	filtered_runs = runs
+	filtered_funruns = funruns
 
 	# THIS NEEDS TO BE CHANGED TO BE MORE GENERIC AND REUSABLE
 	for k in filtered_params:
 		if k == 'theme' or k == 'challenge' or k == 'location':
-			filtered_runs = api_helpers.select(runs, (lambda e : p in e[k + str('s')] for p in filtered_params[k]))
+			filtered_funruns = api_helpers.select(funruns, (lambda e : p in e[k + str('s')] for p in filtered_params[k]))
 		elif k == 'min_price' or k == 'min_length':
-			filtered_runs = api_helpers.select(runs, lambda e : filtered_params[k] <= e[k])
+			filtered_funruns = api_helpers.select(funruns, lambda e : filtered_params[k] <= e[k])
 		elif k == 'max_price' or k == 'max_length':
-			filtered_runs = api_helpers.select(runs, lambda e : e[k] <= filtered_params[k])
+			filtered_funruns = api_helpers.select(funruns, lambda e : e[k] <= filtered_params[k])
 
-	return jsonify({'runs': filtered_runs})
+	return jsonify({'funruns': filtered_funruns})
 
-@runs_api.route('/runs/<int:id>/', methods = ['GET'])
+@funruns_api.route('/funruns/<int:id>/', methods = ['GET'])
 def get_run_by_id(id):
     print "Help"
-    runs = api_helpers.retrieve_runs()
-    return jsonify({'run': runs[id]})
+    funruns = api_helpers.retrieve_funruns()
+    return jsonify({'run': funruns[id]})
 
-@runs_api.route('/runs/<int:id>/themes/', methods = ['GET'])
+@funruns_api.route('/funruns/<int:id>/themes/', methods = ['GET'])
 def get_run_themes(id):
 	# ASSUMING THEMES IS THE LIST OF THEMES
-	# ASSUMING RUNS IS THE ENTIRE LIST OF RUNS
-	runs = api_helpers.retrieve_runs()
+	# ASSUMING FUNRUNS IS THE ENTIRE LIST OF FUNRUNS
+	funruns = api_helpers.retrieve_funruns()
 	themes = api_helpers.retrieve_themes()
 	chosen_themes = []
-	for i in runs[id]['themes']:
+	for i in funruns[id]['themes']:
 		chosen_themes += [themes[i]]
 	return jsonify({'themes': chosen_themes})
 
-@runs_api.route('/runs/<int:id>/challenges/', methods = ['GET'])
+@funruns_api.route('/funruns/<int:id>/challenges/', methods = ['GET'])
 def get_run_challenges(id):
 	# ASSUMING CHALLENGES IS THE ENTIRE LIST OF CHALLENGES
-	# ASSUMING RUNS IS THE ENTIRE LIST OF RUNS
-	runs = api_helpers.retrieve_runs()
+	# ASSUMING FUNRUNS IS THE ENTIRE LIST OF FUNRUNS
+	funruns = api_helpers.retrieve_funruns()
 	challenges = api_helpers.retrieve_challenges()
 	chosen_challenges = []
-	for i in runs[id]['challenges']:
-		print str(i)
+	for i in funruns[id]['challenges']:
 		chosen_challenges += [challenges[i]]
 	return jsonify({'challenges': chosen_challenges})
 
