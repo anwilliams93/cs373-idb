1c39785b1d9b53cb72ae88f8c5ecb31d4557d0b1
Kurt Bixby <kurtbixby@utexas.edu>
2015-03-25 20:55:01 -0500

    Started implementation of the API. We need unit tests.

---
+++ app/api_helpers.py
@@ -0,0 +1,9 @@
+def filter_query_parameters(allowed_parameters, request_parameters):
+	filtered_parameters = {}
+	for k in request_parameters:
+		if k in allowed_parameters:
+			filtered_parameters[k] = request_parameters[k]
+	return filtered_parameters
+
+def select(table, predicate):
+	return filter(predicate, table)
