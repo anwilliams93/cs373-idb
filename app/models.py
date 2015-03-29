6dfa050ae90beffac8c5980f2b7c89ce38ef30b8
David Aguirre <da9492@cs.utexas.edu>
2015-03-29 16:18:58 -0500

    modify models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bear:GryllsBurgers@swe-victini/models'
 db = SQLAlchemy(app)
 
 # --------------------------------
