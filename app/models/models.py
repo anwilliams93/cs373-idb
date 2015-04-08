60b35d813ae3efa52ffd05f7f81b6c92a92b29b1
David Aguirre <da9492@cs.utexas.edu>
2015-04-07 19:28:03 -0500

    models debugging with all tables added

--- app/models/models.py
+++ app/models/models.py
@@ -177,7 +177,7 @@ db.session.commit()
 
 chal1 = Challenge(0, '1', '2', '3')
 db.session.add(chal1)
-theme1 = Theme(0, '1', '2', '3', 0)
+theme1 = Theme(0, '1', '2', '3', [0])
 db.session.add(theme1)
 loc1 = Location(0, 'blah', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'LANDMARK')
 db.session.add(loc1)
