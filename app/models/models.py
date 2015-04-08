8064c7703d700f65a2caa7bca8cbd6d0951c764d
David Aguirre <da9492@cs.utexas.edu>
2015-04-07 19:30:45 -0500

    models debugging with all tables added

--- app/models/models.py
+++ app/models/models.py
@@ -177,11 +177,11 @@ db.session.commit()
 
 chal1 = Challenge(0, '1', '2', '3')
 db.session.add(chal1)
-theme1 = Theme(0, '1', '2', '3', 0)
+theme1 = Theme(0, '1', '2', '3')
 db.session.add(theme1)
 loc1 = Location(0, 'blah', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'LANDMARK')
 db.session.add(loc1)
-frun1 = FunRun(0, 'name', 'addr', 'date', 'dist', 'price', 'hosts', 'spons', 'charit', 'web', 'desc', 'map', 0, 0, 0)
+frun1 = FunRun(0, 'name', 'addr', 'date', 'dist', 'price', 'hosts', 'spons', 'charit', 'web', 'desc', 'map', 0)
 db.session.add(frun1)
 #print("whatjkalsdjfkla;sjdfkl;asdf\n\n\n")
 
