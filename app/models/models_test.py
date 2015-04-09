e53d1bad601672fa3623b05f45217e082a5f1a07
David Aguirre <da9492@cs.utexas.edu>
2015-04-09 18:40:58 -0500

    moddel testsing

--- app/models/models_test.py
+++ app/models/models_test.py
@@ -1,21 +1,21 @@
 
 from models.models import db, Location, FunRun, Theme, Challenge
 import os
-import sql_db
+#import sql_db
 import unittest
 import tempfile
 
 class sql_dbTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.db_fd, sql_db.app.config['DATABASE'] = tempfile.mkstemp()
-        sql_db.app.config['TESTING'] = True
-        self.app = sql_db.app.test_client()
-        sql_db.init_db()
+    # def setUp(self):
+    #     self.db_fd, sql_db.app.config['DATABASE'] = tempfile.mkstemp()
+    #     sql_db.app.config['TESTING'] = True
+    #     self.app = sql_db.app.test_client()
+    #     sql_db.init_db()
 
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(sql_db.app.config['DATABASE'])
+    # def tearDown(self):
+    #     os.close(self.db_fd)
+    #     os.unlink(sql_db.app.config['DATABASE'])
 
     def test_add_funrun(self):
     	result = db.session.query(FunRun).count()
