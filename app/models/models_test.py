7dca4ef9ff0d33136166bc2ae2f3f71c5acbecc6
David Aguirre <da9492@cs.utexas.edu>
2015-04-09 19:26:42 -0500

    moddel testsing

--- app/models/models_test.py
+++ app/models/models_test.py
@@ -38,14 +38,11 @@ class sql_dbTestCase(unittest.TestCase):
     	db.session.add(frun1)
     	result = db.session.query(FunRun).count()
     	assert (result == 13)
-
-    def test_delete_funrun(self):
-    	frun1 = FunRun(id = 13, name = 'testing')
-    	db.session.add(frun1)
     	db.session.delete(frun1)
     	db.session.flush()
     	result = db.session.query(FunRun).count()
     	assert (result == 12)
+
 
 
 
