ccb8ce2fdcf6f3c89afe5ad9ff74e95010649f10
David Aguirre <da9492@cs.utexas.edu>
2015-04-09 20:53:39 -0500

    Testing

--- app/models/models_test.py
+++ app/models/models_test.py
@@ -7,15 +7,6 @@ import tempfile
 
 class sql_dbTestCase(unittest.TestCase):
 
-	# def setUp(self):
-	#     self.db_fd, sql_db.app.config['DATABASE'] = tempfile.mkstemp()
-	#     sql_db.app.config['TESTING'] = True
-	#     self.app = sql_db.app.test_client()
-	#     sql_db.init_db()
-
-	# def tearDown(self):
-	#     os.close(self.db_fd)
-	#     os.unlink(sql_db.app.config['DATABASE'])
 
 	def test_funrun(self):
 		result = db.session.query(FunRun).count()
