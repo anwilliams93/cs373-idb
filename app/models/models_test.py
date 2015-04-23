e47f264aac8772c45609efc6658e3d16cdaeb643
Bear Grylls <anwilliams93@gmail.com>
2015-04-23 04:01:33 +0000

    Got tests working finally

--- app/models/models_test.py
+++ app/models/models_test.py
@@ -7,138 +7,165 @@ import tempfile
 
 class TestDBModels(unittest.TestCase):
 
-
 	def test_funrun(self):
-		result = db.session.query(FunRun).count()
-		assert (result == 12)
+		print("TESTING1")
+		result = db.session.query(FunRun.id).count()
+		self.assertEqual(result, 12)
 
 	def test_themes(self):
-		result = db.session.query(Theme).count()
-		assert (result == 10)
+		print("TESTING2")
+		result = db.session.query(Theme.id).count()
+		self.assertEqual(result, 10)
 
 	def test_challenges(self):
-		result = db.session.query(Challenge).count()
-		assert (result == 16)
+		print("TESTING3")
+		result = db.session.query(Challenge.id).count()
+		self.assertEqual(result, 16)
 
 	def test_location(self):
-		result = db.session.query(Location).count()
-		assert (result == 10)
+		print("TESTING4")
+		result = db.session.query(Location.id).count()
+		self.assertEqual(result, 10)
 
 	def test_add_funrun(self):
+		print("TESTING5")
 		frun1 = FunRun(id = 13, name = 'testing')
 		db.session.add(frun1)
-		result = db.session.query(FunRun).count()
-		assert (result == 13)
+		result = db.session.query(FunRun.id).count()
+		self.assertEqual (result, 13)
 		db.session.delete(frun1)
 		db.session.flush()
-		result = db.session.query(FunRun).count()
-		assert (result == 12)
+		result = db.session.query(FunRun.id).count()
+		self.assertEqual (result, 12)
+		db.session.close()		
 
 	def test_add_themes(self):
+		print("TESTING6")
 		theme1 = Theme(id = 11, name = 'testing')
 		db.session.add(theme1)
-		result = db.session.query(Theme).count()
-		assert (result == 11)
+		result = db.session.query(Theme.id).count()
+		self.assertEqual (result, 11)
 		db.session.delete(theme1)
 		db.session.flush()
-		result = db.session.query(Theme).count()
-		assert (result == 10)
+		result = db.session.query(Theme.id).count()
+		self.assertEqual (result, 10)
+		db.session.close()		
 
 	def test_add_challenge(self):
+		print("TESTING7")
 		chal1 = Challenge(id = 17, name = 'testing')
 		db.session.add(chal1)
-		result = db.session.query(Challenge).count()
-		assert (result == 17)
+		result = db.session.query(Challenge.id).count()
+		self.assertEqual (result, 17)
 		db.session.delete(chal1)
 		db.session.flush()
-		result = db.session.query(Challenge).count()
-		assert (result == 16)
+		result = db.session.query(Challenge.id).count()
+		self.assertEqual (result, 16)
+		db.session.close()
 
 	def test_add_location(self):
+		print("TESTING8")
 		location1 = Location(id = 11, name = 'testing')
 		db.session.add(location1)
-		result = db.session.query(Location).count()
-		assert (result == 11)
+		result = db.session.query(Location.id).count()
+		self.assertEqual (result, 11)
 		db.session.delete(location1)
 		db.session.flush()
-		result = db.session.query(Location).count()
-		assert (result == 10)
+		result = db.session.query(Location.id).count()
+		self.assertEqual (result, 10)
+		db.session.close()		
 
 	def test_fun_location_relationship_1(self):
+		print("TESTING9")
 		results = db.session.query(FunRun).order_by(FunRun.id)
-		assert(results[0].location_id ==  1)
+		self.assertEqual(results[0].location_id,  1)
 
 	def test_fun_location_relationship_2(self):
+		print("TESTING10")
 		results = db.session.query(FunRun).order_by(FunRun.id)
-		assert(results[1].location_id ==  3)
+		self.assertEqual(results[1].location_id,  3)
 
 	def test_fun_location_relationship_3(self):
+		print("TESTING11")
 		results = db.session.query(FunRun).order_by(FunRun.id)
-		assert(results[2].location_id ==  2)
+		self.assertEqual(results[2].location_id,  2)
 
 	def test_fun_location_relationship_4(self):
+		print("TESTING12")
 		results = db.session.query(FunRun).order_by(FunRun.id)
-		assert(results[3].location_id ==  7)
+		self.assertEqual(results[3].location_id,  7)
 
 	def test_fun_theme_relationship_1(self):
+		print("TESTING13")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		theme = db.session.query(Theme).order_by(Theme.id)
-		assert(results[0].funRun_theme[0] == theme[1])
+		self.assertEqual(results[0].funRun_theme[0], theme[1])
 
 	def test_fun_theme_relationship_2(self):
+		print("TESTING14")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		theme = db.session.query(Theme).order_by(Theme.id)
-		assert(results[1].funRun_theme[0] == theme[0])
+		self.assertEqual(results[1].funRun_theme[0], theme[0])
 
 	def test_fun_theme_relationship_3(self):
+		print("TESTING15")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		theme = db.session.query(Theme).order_by(Theme.id)
-		assert(results[2].funRun_theme[0] == theme[0])
+		self.assertEqual(results[2].funRun_theme[0], theme[0])
 
 	def test_fun_theme_relationship_4(self):
+		print("TESTING16")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		theme = db.session.query(Theme).order_by(Theme.id)
-		assert(results[2].funRun_theme[1] == theme[2])
+		self.assertEqual(results[2].funRun_theme[1], theme[2])
 
 	def test_fun_challenge_relationship_1(self):
+		print("TESTING17")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[7].funRun_challenge[0] == challenge[6])
+		self.assertEqual(results[7].funRun_challenge[0], challenge[6])
 
 	def test_fun_challenge_relationship_2(self):
+		print("TESTING18")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[0].funRun_challenge[1] == challenge[3])
+		self.assertEqual(results[0].funRun_challenge[1], challenge[3])
 
 	def test_fun_challenge_relationship_3(self):
+		print("TESTING19")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[1].funRun_challenge[0] == challenge[1])
+		self.assertEqual(results[1].funRun_challenge[0], challenge[1])
 
 	def test_fun_challenge_relationship_3(self):
+		print("TESTING20")
 		results = db.session.query(FunRun).order_by(FunRun.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[2].funRun_challenge[0] == challenge[1])
+		self.assertEqual(results[2].funRun_challenge[0], challenge[1])
 
 	def test_theme_challenge_relationship_1(self):
+		print("TESTING21")
 		results = db.session.query(Theme).order_by(Theme.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[0].theme_challenge[0] == challenge[1])
+		self.assertEqual(results[0].theme_challenge[0], challenge[1])
 	
 	def test_theme_challenge_relationship_2(self):
+		print("TESTING22")
 		results = db.session.query(Theme).order_by(Theme.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[0].theme_challenge[1] == challenge[2])
+		self.assertEqual(results[0].theme_challenge[1], challenge[2])
 
 	def test_theme_challenge_relationship_3(self):
+		print("TESTING23")
 		results = db.session.query(Theme).order_by(Theme.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[3].theme_challenge[0] == challenge[3])
+		self.assertEqual(results[3].theme_challenge[0], challenge[3])
 
 	def test_theme_challenge_relationship_4(self):
+		print("TESTING24")
 		results = db.session.query(Theme).order_by(Theme.id)
 		challenge = db.session.query(Challenge).order_by(Challenge.id)
-		assert(results[2].theme_challenge[0] == challenge[1])
+		self.assertEqual(results[2].theme_challenge[0], challenge[1])
 
 if __name__ == '__main__':
 	unittest.main()
--- app/run_tests.py
+++ app/run_tests.py
@@ -2,49 +2,33 @@ from api.api_test import TestAPI
 from models.models_test import TestDBModels
 import unittest
 import sys
+import threading
 from io import StringIO
 
-def runTests():
-	# Keep it ugly for now so I can figure out how to do it without a file
+def run_api_tests():
+	catch_api_output = StringIO()
 
-	# stream = StringIO()
-	# runner = unittest.TextTestRunner(stream = stream)
-	# result = runner.run(unittest.makeSuite(TestAPI))
-	# stream.seek(0)
-	# return stream.read()
-	print("///////////////////////////////////////////// OPEN API OUTPUT FILE /////////////////////////////////////////////")
-	# oldStdOut = sys.stdout
-	catch_api_output = open('api/test_output.txt', "w")
 	print("///////////////////////////////////////////// LOAD API TESTS /////////////////////////////////////////////")
 
-	# catch_api_output = StringIO()
 	api_tests = unittest.TestLoader().loadTestsFromTestCase(TestAPI)
 	print("///////////////////////////////////////////// RUN API TESTS /////////////////////////////////////////////")
 
 	unittest.TextTestRunner(stream=catch_api_output, verbosity=2).run(api_tests)
-	print("///////////////////////////////////////////// CLOSE API OUTPUT FILE /////////////////////////////////////////////")
 
-	# catch_api_output.write("hello")
-	# sys.stdout = oldStdOut
-	# print("Hi" + catch_api_output.getvalue())
-	catch_api_output.close()
+	return catch_api_output
 
-	# return catch_api_output.getvalue()
-
-	print("///////////////////////////////////////////// OPEN MODELS OUTPUT FILE /////////////////////////////////////////////")
-	# Models
-	catch_model_output = open('models/test_output.txt', "w")
+def run_models_tests():
+	catch_model_output = StringIO()
 	print("///////////////////////////////////////////// LOAD MODELS TESTS /////////////////////////////////////////////")
 
 	model_tests = unittest.TestLoader().loadTestsFromTestCase(TestDBModels)
 	print("///////////////////////////////////////////// RUN MODELS TESTS /////////////////////////////////////////////")
 
 	unittest.TextTestRunner(stream=catch_model_output, verbosity=2).run(model_tests)
-	print("///////////////////////////////////////////// CLOSE MODELS OUTPUT FILE /////////////////////////////////////////////")
 
-	catch_model_output.close()
 	print("///////////////////////////////////////////// TESTS DONE /////////////////////////////////////////////")	
 
+	return catch_model_output
 
 if __name__ == '__main__':
 	runTests()
